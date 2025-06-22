c2ea903c9d193db69168d34c9244a6b41d51b17c
AlexMuna39 <acm3927@gmail.com>
2025-06-22 18:23:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 import os
 
+from app.routes import router
+
 app = FastAPI()
+
+app.include_router(router)
 
 @app.get("/")
 def read_root():
@@ -10,4 +14,4 @@ def read_root():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
