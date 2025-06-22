f264f126689f69c9ce1fe7b027acfdcaa907636f
AlexMuna39 <acm3927@gmail.com>
2025-06-22 19:38:21 -0300

    Update and rename temp.txt to task.py

---
+++ app/models/task.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class Task(BaseModel):
+    id: int
+    title: str
+    description: Optional[str] = None
+    created_at: datetime
+    completed: bool = False
