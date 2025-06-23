c18ee167e8568779ddb7c42e3715d8489b9571e7
AlexMuna39 <acm3927@gmail.com>
2025-06-23 01:31:05 -0300

    Update and rename app/route/__init__.py to route/__init__.py

--- route/__init__.py
+++ route/__init__.py
@@ -2,7 +2,5 @@ from fastapi import APIRouter
 from route import healthcheck, task_route
 
 router = APIRouter()
-
-# Include all routers here
 router.include_router(healthcheck.router)
 router.include_router(task_route.router)
