cbb3aef2e0829efb200d5a281dd9b5c2c8669903
AlexMuna39 <acm3927@gmail.com>
2025-06-23 00:55:40 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
-from app.route import healthcheck
-from app.route import task_route
+from route import router
 
 app = FastAPI()
-app.include_router(healthcheck.router, prefix="/health")
 
-# Model for Tasks
-class Task(BaseModel):
-    id: int
-    title: str
-    description: str
-    done: bool
-
-# Dummy in-memory database
-tasks: List[Task] = []
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, this is Eva's Task Assistant API!"}
-
-@app.get("/tasks")
-def get_tasks():
-    return tasks
-
-@app.post("/tasks")
-def create_task(task: Task):
-    tasks.append(task)
-    return task
+app.include_router(router)
