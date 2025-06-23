f56317e4a9464b3c5fe23d2e9532af18de592b2b
AlexMuna39 <acm3927@gmail.com>
2025-06-22 23:22:08 -0300

    Update __init__.py

--- app/route/__init__.py
+++ app/route/__init__.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+from app.routes import healthcheck, task_routes
 
+router = APIRouter()
+
+# Include all routers here
+router.include_router(healthcheck.router)
+router.include_router(task_routes.router)
