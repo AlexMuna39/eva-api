f6bdc8eb49691bda4a33ace4138356c98b12682b
AlexMuna39 <acm3927@gmail.com>
2025-06-23 01:03:02 -0300

    Update task_service.py

--- app/service/task_service.py
+++ app/service/task_service.py
@@ -1,12 +1,15 @@
-from app.model.task_model import Task
-from app.service.supabase_service import SupabaseService
+from model.task_model import Task
+
+# Temporary in-memory storage
+tasks_db = []
 
 class TaskService:
 
     @staticmethod
     def create_task(task: Task):
-        return SupabaseService.create_task(task)
+        tasks_db.append(task)
+        return task
 
     @staticmethod
     def list_tasks():
-        return SupabaseService.list_tasks()
+        return tasks_db
