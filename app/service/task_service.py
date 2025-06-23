3e780124e4c376553e5cfe2db35edc9d9ced5e91
AlexMuna39 <acm3927@gmail.com>
2025-06-22 23:23:35 -0300

    Update task_service.py

--- app/service/task_service.py
+++ app/service/task_service.py
@@ -1,1 +1,14 @@
+from app.models.task_models import Task
 
+# Temporary in-memory storage
+tasks_db = []
+
+class TaskService:
+    @staticmethod
+    def create_task(task: Task):
+        tasks_db.append(task)
+        return task
+
+    @staticmethod
+    def list_tasks():
+        return tasks_db
